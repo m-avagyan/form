6f89ea0e17a945a1daed7a70388355e5a0524bbf
m-avagyan <avagyankristina44@gmail.com>
2020-07-26 20:59:57 +0400

    Updates

---
+++ precache-manifest.afe7ce6ba762b32e43f58e08f35100bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d019d66072907d381f746146b54361",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "e66f7cf691e6cc30a3d4",
+    "url": "/form/static/css/main.fca15ef5.chunk.css"
+  },
+  {
+    "revision": "e15e5812049ae2554985",
+    "url": "/form/static/js/2.d419d6ad.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/form/static/js/2.d419d6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66f7cf691e6cc30a3d4",
+    "url": "/form/static/js/main.84e1122f.chunk.js"
+  },
+  {
+    "revision": "88bc9024cd2a3308059e",
+    "url": "/form/static/js/runtime-main.fd21892c.js"
+  },
+  {
+    "revision": "c66b6b6fcf29895cc7d8323c9a82c098",
+    "url": "/form/static/media/bg.c66b6b6f.jpg"
+  }
+]);
